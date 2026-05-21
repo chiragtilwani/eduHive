3c7428b72391860ce427b880927bf55e3bca7d77
Chirag <chiragtilwani08@gmail.com>
2026-05-22 02:23:52 +0530

    Remove explicit values from CourseCategoryEnum members
    The numeric values assigned to the `CourseCategoryEnum` members
    have been removed. Enum members now rely on implicit values
    assigned by the compiler, starting from 0 and incrementing by 1
    for each subsequent member. This simplifies the enum definition
    and reduces redundancy in the code.

--- DataStore.Abstraction/Enums/CourseCategoryEnum.cs
+++ DataStore.Abstraction/Enums/CourseCategoryEnum.cs
@@ -3,14 +3,14 @@
     public enum CourseCategoryEnum
     {
         Programming = 1,
-        DataScience = 2,
-        Design = 3,
-        Marketing = 4,
-        Business = 5,
-        PersonalDevelopment = 6,
-        HealthAndFitness = 7,
-        Music = 8,
-        LanguageLearning = 9,
-        FinanceAndInvesting = 10
+        DataScience,
+        Design,
+        Marketing,
+        Business,
+        PersonalDevelopment,
+        HealthAndFitness,
+        Music,
+        LanguageLearning,
+        FinanceAndInvesting
     }
 }
